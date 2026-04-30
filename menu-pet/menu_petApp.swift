8e5558837e5e8682e0a9da4fc5ed5b700f96377b
fourmietsouris <catcher.in.the.rye.v@gmail.com>
2026-04-30 14:08:39 +0700

    Initial Commit

---
+++ menu-pet/menu_petApp.swift
@@ -0,0 +1,17 @@
+//
+//  menu_petApp.swift
+//  menu-pet
+//
+//  Created by icosohedral on 2026/4/30.
+//
+
+import SwiftUI
+
+@main
+struct menu_petApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
